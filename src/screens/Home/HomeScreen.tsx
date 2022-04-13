9cefb6f7ab7ad901ebe8912334a3dfcd0ced0ee3
Dominic Sherman <dominic.sherman98@gmail.com>
2022-04-13 08:00:59 -0700

    more wips

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { MY_ENV_VALUE, NEW_VALUE } from '@env';
 
 import { Screen } from '@/components/Screen';
 import { Text } from '@/components/Text';
@@ -9,7 +10,8 @@ import { Text } from '@/components/Text';
 export const HomeScreen: FC = () => {
   return (
     <Screen testID="HomeScreen" alignItems="center" justifyContent="center">
-      <Text>{`MY_ENV_VALUE: ${process.env.MY_ENV_VALUE}`}</Text>
+      <Text>{`MY_ENV_VALUE: ${MY_ENV_VALUE}`}</Text>
+      <Text>{`NEW_VALUE: ${NEW_VALUE}`}</Text>
     </Screen>
   );
 };
